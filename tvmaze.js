ef67850315d752298ebe6a37a5892a7f4d0a6389
prokid20000 <95936374+prokid20000@users.noreply.github.com>
2022-01-19 15:15:15 -0500

    Completed populateshows

--- tvmaze.js
+++ tvmaze.js
@@ -3,6 +3,8 @@
 const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
+const TVMAZE_BASEURL = 'http://api.tvmaze.com/';
+const MISSING_URL = `https://tinyurl.com/tv-missing`;
 
 /** Given a search term, search for tv shows that match that query.
  *
@@ -13,23 +15,26 @@ const $searchForm = $("#searchForm");
 
 async function getShowsByTerm(term) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
-  let response = await axios.get(`http://api.tvmaze.com/search/shows/?`, {
+  let response = await axios.get(`${TVMAZE_BASEURL}search/shows/?`, {
     params: { q: term },
   });
   console.log("response: ", response);
   let showInfo = [];
 
+  //Iterate through each show object to get necessary data and store in new object which will go to array.
   for (let showIndex in response.data) {
     let showObject = {};
     showObject["id"] = response.data[showIndex].show.id;
     showObject["name"] = response.data[showIndex].show.name;
     showObject["summary"] = response.data[showIndex].show.summary;
-    if(response.data[showIndex].show.image === null){
-      showObject["image"] = `https://tinyurl.com/tv-missing`;
+
+    //If there's no image, it will set as default 'missing' image.
+    if (response.data[showIndex].show.image === null) {
+      showObject["image"] = MISSING_URL;
     } else {
       showObject["image"] = response.data[showIndex].show.image.medium;
     }
-    
+
     console.log("showObject: ", showObject);
     showInfo.push(showObject);
   }
@@ -40,14 +45,15 @@ async function getShowsByTerm(term) {
 
 function populateShows(shows) {
   $showsList.empty();
-
+  //Iterate through array of shows to populate every show found in search.
   for (let show of shows) {
+    //Populate DOM with information from show object(id, image, name, summary).
     const $show = $(
       `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img 
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg" 
-              alt="Bletchly Circle San Francisco" 
+              src="${show.image}" 
+              alt="${show.name}" 
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
@@ -78,6 +84,7 @@ async function searchForShowAndDisplay() {
   populateShows(shows);
 }
 
+/** Once button 'search' is clicked, shows will be searched and displayed. */
 $searchForm.on("submit", async function (evt) {
   evt.preventDefault();
   await searchForShowAndDisplay();
